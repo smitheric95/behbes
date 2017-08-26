346d7785c2acde1488c9714548f3c7bc6859181f
Arslan Memon <arslanmemon@Arslans-MBP.home>
2017-08-25 23:18:41 -0500

    added geetdata path

--- webApp/src/routes.php
+++ webApp/src/routes.php
@@ -1,10 +1,17 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/Home[/{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/getData', function ($request, $response, $args) {
+    // Sample log message
+   $arr = array ('a'=>"eseghrrnehreh",'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+
+    return json_encode($arr);
 });
