51380e993791ec545eecdcc12b5ebe2fc104700e
aWiedebusch <awiedebusch@smu.edu>
2017-08-30 14:28:26 -0500

    TS configured

--- webApp/public/myApp/src/app/app.module.ts
+++ webApp/public/myApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { FormPage } from '../pages/form/form';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -26,7 +27,8 @@ import { FormService } from './services/form/form.service';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    FormPage
   ],
   providers: [
     StatusBar,
