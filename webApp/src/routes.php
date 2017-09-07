e58c42a7b128ab8e8e74809c26719ef346b3f4ec
Arslan Memon <arslanmemon@Arslans-MacBook-Pro.local>
2017-09-07 15:02:45 -0500

    got freaking data

--- webApp/src/routes.php
+++ webApp/src/routes.php
@@ -67,7 +67,7 @@ $app->post('/postform',function($request,$response){
 	$input = json_decode($input,true);
 	$parsed_array = [];
 	foreach ($input as $key => $value){
-		$stmt = $this->db->prepare("SELECT SymptomID FROM Symptoms WHERE Description = :Description")；
+		$stmt = $this->db->prepare("SELECT SymptomID FROM Symptoms WHERE Description = :Description");
 		$stmt->bindValue(':Description', $value, PDO::PARAM_STR);
 		try{
 				$stmt->execute();
@@ -76,10 +76,15 @@ $app->post('/postform',function($request,$response){
 					return $this->response->withStatus(400);
 			}
 		$idinfo = $stmt->fetchAll();
-		array_push($parsed_array, $idinfo[0][0]);
+		array_push($parsed_array, $idinfo[0]["SymptomID"]);
 	}
-	$in_values = implode(',', $parsed_arrary);
-	$stmt = $this->db->prepare("SELECT s1.IllnessID, COUNT(s1.SymptomID) AS counted FROM  SymptomIllness as s1 INNER JOIN Symptoms as s2 ON s1.SymptomID=s2.SymptomID INNER JOIN Illnesses as i1 ON s1.IllnessID = i1.IllnessID WHERE s1.SymptomNameID IN (".$in_values.") GROUP BY s1.IllnessID ORDER BY counted DESC");
+	$str = "";
+	foreach ($parsed_array as $i){
+      $str = $str.(string)$i;
+      $str = $str.',';
+	}
+    print($str);
+	$stmt = $this->db->prepare("SELECT s1.IllnessID, COUNT(s1.SymptomID) AS counted FROM  SymptomIllness as s1 INNER JOIN Symptoms as s2 ON s1.SymptomID=s2.SymptomID INNER JOIN Illnesses as i1 ON s1.IllnessID = i1.IllnessID WHERE s1.SymptomID IN (".$str." 0) GROUP BY s1.IllnessID ORDER BY counted DESC");
 	try{
 				$stmt->execute();
 			}
