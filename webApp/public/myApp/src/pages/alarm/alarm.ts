802f7e193e0945709a6a2a55ef975deff9523ee5
Jeffrey Tang <jktang@smu.edu>
2017-09-07 04:20:54 -0500

    Initial commit

---
+++ webApp/public/myApp/src/pages/alarm/alarm.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { FormService } from '../../app/services/form/form.service';
+
+import { IllnessPage } from '../illness/illness';
+
+@Component({
+  selector: 'page-alarms',
+  templateUrl: 'alarm.html'
+})
+export class AlarmsPage {
+  
+  causes: any[];
+  cause: any[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formService: FormService) {
+    this.causes = navParams.get('response');
+    this.cause = [];
+  }
+
+
+  //sends the other possible causes to return to this page
+  async causeTapped(event, cause_name) {
+
+    Promise.all(this.cause = await this.formService.getIllness(cause_name))
+      .then(value => this.pushPage(cause_name));
+  }
+
+  pushPage(cause_name) {
+    this.navCtrl.push(IllnessPage, {
+      cause: this.cause,
+      cause_name: cause_name
+    });
+  }
+}
